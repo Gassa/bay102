c919423b8eb32c18af23fd3c643d1ceddd2a1f86
Gassa Yan <gassa0617@gmail.com>
2022-05-22 18:01:08 -0700

    split backend and frontend

---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import * as path from 'path';
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+app.get('/backend', (req: Request, res: Response) => {
+  // res.send('Express + TypeScript Server');
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
