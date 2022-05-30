68b42c110fd0a856a64f3523e1b323475cc6c4e6
Gassa Yan <gassa0617@gmail.com>
2022-05-30 14:20:11 -0700

    fix padding for room container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Container from '@mui/material/Container'
 import { Pages } from './pages/Pages'
@@ -7,13 +7,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
+
+
 function App() {
 
   return (
     <React.Fragment>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-      <Container maxWidth="sm">
+      <Container maxWidth="lg" sx = {{ padding: '0px 0px' }}>
         <BrowserRouter>
           <Pages />
         </BrowserRouter>
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Container from "@mui/material/Container";
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 export default function PageLayout() {
   return (
-    <Container maxWidth="sm">
+    <React.Fragment>
         <Header />
         <Outlet />
         <Footer /> 
-    </Container>
+    </React.Fragment>
   );
 }
