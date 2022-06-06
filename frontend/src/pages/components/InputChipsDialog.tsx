264077ad6e393e79c447777fe643580deaa8f0e4
Gassa Yan <gassa0617@gmail.com>
2022-06-06 13:32:39 -0700

    improve input dialog

--- frontend/src/pages/components/InputChipsDialog.tsx
+++ frontend/src/pages/components/InputChipsDialog.tsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Dialog from '@mui/material/Dialog';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Avatar from '@mui/material/Avatar';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Dialog from "@mui/material/Dialog";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import * as _ from 'lodash';
 
-const chips = [ '1', '5', '25', '100', '500' ];
-const initMap = { '1': 0, '5': 0, '25': 0, '100': 0, '500': 0 }
+const chips = ["1", "5", "25", "100", "500"];
+const initMap = { "1": 0, "5": 0, "25": 0, "100": 0, "500": 0 };
 
 export interface ConfirmationDialogRawProps {
   id: string;
@@ -20,10 +21,10 @@ export interface ConfirmationDialogRawProps {
   onClose: (value: number) => void;
 }
 
-export function ConfirmationDialogRaw(props: ConfirmationDialogRawProps) {
+export function InputChipsAmountDialog(props: ConfirmationDialogRawProps) {
   const { onClose, open, ...other } = props;
   const [totalAmount, setTotalAmount] = React.useState(0);
-  const [equation, setEquation] = React.useState('');
+  const [equation, setEquation] = React.useState("");
   const [chipsMap, setChipsMap] = React.useState(initMap);
 
   const handleCancel = () => {
@@ -34,13 +35,17 @@ export function ConfirmationDialogRaw(props: ConfirmationDialogRawProps) {
     onClose(totalAmount);
   };
 
-  const composeEquation = () => Object.entries(chipsMap)
-    .map((item) => { return `${item[0]} x ${item[1]}`})
-    .join(" + ")
-  
+  const composeEquation = () =>
+    _.entries(chipsMap)
+      .map((item) => {
+        return `${item[0]} x ${item[1]}`;
+      })
+      .join(" + ");
 
-  const compuateTotalAmount = () => Object.entries(chipsMap)
-    .reduce((acc, item) => { return acc + parseInt(item[0]) * item[1]}, 0)
+  const compuateTotalAmount = () =>
+    _.entries(chipsMap).reduce((acc, item) => {
+      return acc + parseInt(item[0]) * item[1];
+    }, 0);
 
   const handleEntering = () => {
     setChipsMap(initMap);
@@ -48,19 +53,22 @@ export function ConfirmationDialogRaw(props: ConfirmationDialogRawProps) {
     setEquation("");
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>, chip: string) => {
-    const newValue = parseInt((event.target as HTMLInputElement).value)
-    const newChipsMap = Object.assign(chipsMap, { [chip]: newValue });
-    const newEquation = composeEquation(); 
-    const newTotalAmount = compuateTotalAmount(); 
-    setChipsMap({ ...newChipsMap })
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    chip: string
+  ) => {
+    const newValue = parseInt((event.target as HTMLInputElement).value) || null;
+    const newChipsMap = _.assign(chipsMap, { [chip]: newValue });
+    const newEquation = composeEquation();
+    const newTotalAmount = compuateTotalAmount();
+    setChipsMap({ ...newChipsMap });
     setTotalAmount(newTotalAmount);
     setEquation(newEquation);
   };
 
   return (
     <Dialog
-      sx={{ '& .MuiDialog-paper': { width: '80%', maxHeight: 435 } }}
+      sx={{ "& .MuiDialog-paper": { width: "80%", maxHeight: 435 } }}
       maxWidth="xs"
       TransitionProps={{ onEntering: handleEntering }}
       open={open}
@@ -68,20 +76,31 @@ export function ConfirmationDialogRaw(props: ConfirmationDialogRawProps) {
     >
       <DialogTitle>Input Chips</DialogTitle>
       <DialogContent dividers>
-      {
-        chips.map(chip => {
+        {chips.map((chip) => {
           return (
-            <Box key={`chip_${chip}`} sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Avatar sx={{ color: 'action.active', mr: 1, my: 0.5 }}>{chip}</Avatar>
-              <TextField id={`chip_${chip}`} label="the number of chips" variant="standard" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, chip)} />
+            <Box
+              key={`chip_${chip}`}
+              sx={{ display: "flex", alignItems: "flex-end" }}
+            >
+              <Avatar sx={{ color: "action.active", mr: 1, my: 0.5 }}>
+                {chip}
+              </Avatar>
+              <TextField
+                id={`chip_${chip}`}
+                label="the number of chips"
+                value={_.get(chipsMap, chip) || '' }
+                variant="standard"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleChange(e, chip)
+                }
+              />
             </Box>
-          )
-        })
-      }
-      <Divider variant="middle" />
-      <Typography color="text.primary" variant="body1">
+          );
+        })}
+        <Divider variant="middle" />
+        <Typography color="text.primary" variant="body1">
           total: {equation} = {totalAmount}
-      </Typography>
+        </Typography>
       </DialogContent>
       <DialogActions>
         <Button autoFocus onClick={handleCancel}>
